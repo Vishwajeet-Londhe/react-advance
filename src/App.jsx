b4bfa687623224c0b7ee6ddccf0d4ed5974f560e
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-12 13:36:11 +0530

    a tag but refresh

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1 className='text-4xl text-red-600'>Hello</h1>
+      <nav>
+        <a href="/home">Home</a>
+      </nav>
     </div>
   )
 }
