69e95a65a2d9cb88fc8f4d6495ff5ee6285bf09e
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-12 16:42:26 +0530

    all working well with error solve

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,13 @@ function App() {
   return (
     <div>
       <nav className='mt-10 flex justify-center gap-10'>
-      <Link to="/home">Home</Link>
+      <Link to="/">Home</Link>
       <Link to="/user">User</Link>
       <Link to="/about">About</Link>
       </nav>
 
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/Home' element={<Home/>} />
         <Route path='/user' element={<User/>} />
         <Route path='/about' element={<About/>} />
       </Routes>
