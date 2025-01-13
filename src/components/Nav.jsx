246d511db789c51ca2aecf23898b2b4e1fba19cf
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-13 19:27:51 +0530

    child

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -31,6 +31,43 @@
 // export default Nav
 
 
+// import React from 'react'
+// import { Link, NavLink } from 'react-router-dom'
+
+
+// function Nav() {
+//   return (
+//     <div>
+//       <nav className='mt-10 flex justify-center gap-10'>
+//       <NavLink className={(e)=>{
+//          return [
+//             e.isActive? 'text-red-300': '',
+//             e.isActive? "font-bold" : '',
+//         ].join(" ");
+//       }} to="/">Home</NavLink>
+    
+//       <NavLink className={(e)=>{
+//          return [
+//             e.isActive? 'text-red-300': '',
+//             e.isActive? "font-bold" : '',
+//         ].join(" ");
+//       }} to="/user">User</NavLink>
+      
+//       <NavLink className={(e)=>{
+//         return [
+//             e.isActive? 'text-red-300': '',
+//             e.isActive? "font-bold" : '',
+//         ].join(" ");
+//       }} to="/about">About</NavLink>
+//       </nav>
+//     </div>
+//   )
+// }
+
+// export default Nav
+
+
+
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
@@ -39,26 +76,57 @@ function Nav() {
   return (
     <div>
       <nav className='mt-10 flex justify-center gap-10'>
-      <NavLink className={(e)=>{
-         return [
-            e.isActive? 'text-red-300': '',
-            e.isActive? "font-bold" : '',
-        ].join(" ");
-      }} to="/">Home</NavLink>
-    
-      <NavLink className={(e)=>{
-         return [
-            e.isActive? 'text-red-300': '',
-            e.isActive? "font-bold" : '',
-        ].join(" ");
-      }} to="/user">User</NavLink>
+      <NavLink to="/">
+        {(e)=>{
+            return (
+                <span
+                    className={[
+                        e.isActive? 'text-green-300': '',
+                        e.isActive? "font-bold" : '',
+            
+                    ].join(" ")}
+                >
+                    Home
+                </span>
+            );
+      }}
       
-      <NavLink className={(e)=>{
-        return [
-            e.isActive? 'text-red-300': '',
-            e.isActive? "font-bold" : '',
-        ].join(" ");
-      }} to="/about">About</NavLink>
+      </NavLink>
+    
+      <NavLink to="/user">
+        {(e)=>{
+            return (
+                <span
+                    className={[
+                        e.isActive? 'text-blue-300': '',
+                        e.isActive? "font-bold" : '',
+            
+                    ].join(" ")}
+                >
+                    User
+                </span>
+            );
+      }}
+      
+      </NavLink>
+      
+      <NavLink to="/about">
+        {(e)=>{
+            return (
+                <span
+                    className={[
+                        e.isActive? 'text-red-300': '',
+                        e.isActive? "font-bold" : '',
+            
+                    ].join(" ")}
+                >
+                    About
+                </span>
+            );
+      }}
+      
+      </NavLink>
+
       </nav>
     </div>
   )
