51afb34d82564f6581ccfcccaeeb7ddb4d3d5c15
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-13 18:57:06 +0530

    class

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,36 @@
+// import React from 'react'
+// import { Link, NavLink } from 'react-router-dom'
+
+
+// function Nav() {
+//   return (
+//     <div>
+//       <nav className='mt-10 flex justify-center gap-10'>
+//       <NavLink style={(e)=>{
+//         return {color: e.isActive ? 'green' : '',
+//             fontWeight: e.isActive ? 'bold' : '',
+//         };
+//       }} to="/">Home</NavLink>
+    
+//       <NavLink style={(e)=>{
+//         return {color: e.isActive ? 'green' : '',
+//             fontWeight: e.isActive ? 'bold' : '',
+//         };
+//       }} to="/user">User</NavLink>
+      
+//       <NavLink style={(e)=>{
+//         return {color: e.isActive ? 'green' : '',
+//             fontWeight: e.isActive ? 'bold' : '',
+//         };
+//       }} to="/about">About</NavLink>
+//       </nav>
+//     </div>
+//   )
+// }
+
+// export default Nav
+
+
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
@@ -6,22 +39,25 @@ function Nav() {
   return (
     <div>
       <nav className='mt-10 flex justify-center gap-10'>
-      <NavLink style={(e)=>{
-        return {color: e.isActive ? 'green' : '',
-            fontWeight: e.isActive ? 'bold' : '',
-        };
+      <NavLink className={(e)=>{
+         return [
+            e.isActive? 'text-red-300': '',
+            e.isActive? "font-bold" : '',
+        ].join(" ");
       }} to="/">Home</NavLink>
     
-      <NavLink style={(e)=>{
-        return {color: e.isActive ? 'green' : '',
-            fontWeight: e.isActive ? 'bold' : '',
-        };
+      <NavLink className={(e)=>{
+         return [
+            e.isActive? 'text-red-300': '',
+            e.isActive? "font-bold" : '',
+        ].join(" ");
       }} to="/user">User</NavLink>
       
-      <NavLink style={(e)=>{
-        return {color: e.isActive ? 'green' : '',
-            fontWeight: e.isActive ? 'bold' : '',
-        };
+      <NavLink className={(e)=>{
+        return [
+            e.isActive? 'text-red-300': '',
+            e.isActive? "font-bold" : '',
+        ].join(" ");
       }} to="/about">About</NavLink>
       </nav>
     </div>
